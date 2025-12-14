0c57304acb09ab651fb9d1f5faf910e9b1cd102b
default <miles.aube@stu.ocsb.ca>
2025-12-14 00:08:34 +0000

    fix linter issues

--- main.ts
+++ main.ts
@@ -16,10 +16,8 @@ const userOperation = prompt("Welcome to my calculator program.\nwhich operation
 if (userOperation == "a"){
   let userValue1AsNumber: number;
   let userValue2AsNumber: number;
-  let userValue1AsString: string;
-  let userValue2AsString: string;
-  userValue1AsString = prompt("Please enter your first number") || "0";
-  userValue2AsString = prompt("Please enter your second number") || "0";
+  const userValue1AsString = prompt("Please enter your first number") || "0";
+  const userValue2AsString = prompt("Please enter your second number") || "0";
   userValue1AsNumber = parseInt(userValue1AsString);
   userValue2AsNumber = parseInt(userValue2AsString);
   answer = userValue1AsNumber + userValue2AsNumber
@@ -29,10 +27,8 @@ if (userOperation == "a"){
 if (userOperation == "b") {
   let userValue1AsNumber: number;
   let userValue2AsNumber: number;
-  let userValue1AsString: string;
-  let userValue2AsString: string;
-  userValue1AsString = prompt("Please enter your first number") || "0";
-  userValue2AsString = prompt("Please enter your second number") || "0";
+  const userValue1AsString = prompt("Please enter your first number") || "0";
+  const userValue2AsString = prompt("Please enter your second number") || "0";
   userValue1AsNumber = parseInt(userValue1AsString);
   userValue2AsNumber = parseInt(userValue2AsString);
   answer = userValue1AsNumber - userValue2AsNumber
@@ -42,10 +38,8 @@ if (userOperation == "b") {
 if (userOperation == "c") {
   let userValue1AsNumber: number;
   let userValue2AsNumber: number;
-  let userValue1AsString: string;
-  let userValue2AsString: string;
-  userValue1AsString = prompt("Please enter your first number") || "0";
-  userValue2AsString = prompt("Please enter your second number") || "0";
+  const userValue1AsString = prompt("Please enter your first number") || "0";
+  const userValue2AsString = prompt("Please enter your second number") || "0";
   userValue1AsNumber = parseInt(userValue1AsString);
   userValue2AsNumber = parseInt(userValue2AsString);
   answer = userValue1AsNumber * userValue2AsNumber
@@ -55,10 +49,8 @@ if (userOperation == "c") {
 if (userOperation == "d") {
   let userValue1AsNumber: number;
   let userValue2AsNumber: number;
-  let userValue1AsString: string;
-  let userValue2AsString: string;
-  userValue1AsString = prompt("Please enter your first number") || "0";
-  userValue2AsString = prompt("Please enter your second number") || "0";
+  const userValue1AsString = prompt("Please enter your first number") || "0";
+  const userValue2AsString = prompt("Please enter your second number") || "0";
   userValue1AsNumber = parseInt(userValue1AsString);
   userValue2AsNumber = parseInt(userValue2AsString);
   answer = userValue1AsNumber / userValue2AsNumber
@@ -67,8 +59,7 @@ if (userOperation == "d") {
 
 if (userOperation == "e") {
   let userValue1AsNumber: number;
-  let userValue1AsString: string;
-  userValue1AsString = prompt("Please enter the integer to make absolute") || "0";
+  const userValue1AsString = prompt("Please enter the integer to make absolute") || "0";
   userValue1AsNumber = parseInt(userValue1AsString);
   answer = Math.abs(userValue1AsNumber);
   console.log("The absolute value of ", userValue1AsNumber," is ", answer);
@@ -76,8 +67,7 @@ if (userOperation == "e") {
 
 if (userOperation == "f") {
   let userValue1AsNumber: number;
-  let userValue1AsString: string;
-  userValue1AsString = prompt("Please enter the number you want to round") || "0";
+  const userValue1AsString = prompt("Please enter the number you want to round") || "0";
   userValue1AsNumber = parseFloat(userValue1AsString);
   answer = Math.round(userValue1AsNumber);
   console.log("The rounded value of ", userValue1AsNumber, " is ", answer);
@@ -86,10 +76,8 @@ if (userOperation == "f") {
 if (userOperation == "g") {
   let userValue1AsNumber: number;
   let userValue2AsNumber: number;
-  let userValue1AsString: string;
-  let userValue2AsString: string;
-  userValue1AsString = prompt("Please enter your base number") || "0";
-  userValue2AsString = prompt("Please enter your exponent") || "0";
+  const userValue1AsString = prompt("Please enter your base number") || "0";
+  const userValue2AsString = prompt("Please enter your exponent") || "0";
   userValue1AsNumber = parseInt(userValue1AsString);
   userValue2AsNumber = parseInt(userValue2AsString);
   answer = Math.pow(userValue1AsNumber, userValue2AsNumber)
@@ -98,8 +86,7 @@ if (userOperation == "g") {
 
 if (userOperation == "h") {
   let userValue1AsNumber: number;
-  let userValue1AsString: string;
-  userValue1AsString = prompt("Please enter the number you want to round") || "0";
+  const userValue1AsString = prompt("Please enter the number you want to round") || "0";
   userValue1AsNumber = parseFloat(userValue1AsString);
   answer = Math.sqrt(userValue1AsNumber);
   console.log("The square root of ", userValue1AsNumber, " is ", answer);
